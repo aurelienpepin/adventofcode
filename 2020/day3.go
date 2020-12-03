0c3b6b884de0599e620e473a6dd77f95d848823d
Aurélien Pepin <aurelien.pepin@blablacar.com>
2020-12-03 20:15:54 +0100

    [day3][part2]

--- 2020/day3.go
+++ 2020/day3.go
@@ -22,20 +22,28 @@ func main() {
 		forest = append(forest, scanner.Text())
 	}
 
-	fmt.Println(part1Traversing(forest))
+	// Part 1
+	fmt.Println(part1Traversing(forest, 3, 1))
+
+	// Part 2
+	slope1 := part1Traversing(forest, 1, 1)
+	slope2 := part1Traversing(forest, 3, 1)
+	slope3 := part1Traversing(forest, 5, 1)
+	slope4 := part1Traversing(forest, 7, 1)
+	slope5 := part1Traversing(forest, 1, 2)
+	fmt.Println(slope1 * slope2 * slope3 * slope4 * slope5)
 }
 
-func part1Traversing(forest []string) int {
-	deltaRight := 3
+func part1Traversing(forest []string, deltaRight int, deltaBottom int) int {
 	y := 0
 	trees := 0
 
-	for x := 1; x < len(forest); x++ {
-		y = (y + deltaRight) % len(forest[x])
-
+	for x := 0; x < len(forest); x += deltaBottom {
 		if string(forest[x][y]) == "#" {
 			trees++
 		}
+
+		y = (y + deltaRight) % len(forest[x])
 	}
 
 	return trees
