dd4d024218526dce8f89f349b957d0e251341741
Aurélien Pepin <aurelien.pepin@blablacar.com>
2020-12-10 18:53:28 +0100

    [day10][part1] sort and count deltas

---
+++ 2020/day10/day10.go
@@ -0,0 +1,42 @@
+package day10
+
+import (
+	"io/ioutil"
+	"log"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func common() []int {
+	content, err := ioutil.ReadFile("2020/inputs/day10")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var numbers []int
+	for _, line := range strings.Split(string(content), "\n") {
+		if number, err := strconv.Atoi(line); err == nil {
+			numbers = append(numbers, number)
+		} else {
+			log.Fatal(err)
+		}
+	}
+
+	return numbers
+}
+
+func Part1() int {
+	adapters := common()
+	sort.Ints(adapters)
+
+	differences := make(map[int]int)
+	differences[adapters[0]]++
+
+	for i := 1; i < len(adapters); i++ {
+		differences[adapters[i] - adapters[i - 1]]++
+	}
+
+	differences[3]++
+	return differences[1] * differences[3]
+}
