ed02c660e429e0b961a5ddb14a20d936d1c22b23
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-01 21:50:23 +0100

    [day1][part2] imperative

--- 2021/src/Day01.scala
+++ 2021/src/Day01.scala
@@ -5,10 +5,26 @@ object Day01 extends App {
       .length
   }
 
+  def solve2(depths: Array[Int]): Int = {
+    var currentSum: Int = depths.take(3).sum
+    var increasing: Int = 0
+
+    for (i <- 3 to depths.length - 1) {
+      val newSum = currentSum + depths(i) - depths(i - 3)
+      if (newSum > currentSum)
+        increasing += 1
+
+      currentSum = newSum
+    }
+
+    increasing
+  }
+
   val input = scala.io.Source.fromFile("inputs/day01")
   var integers = (try input.mkString finally input.close())
     .split('\n')
     .map(_.toInt)
 
-  println(solve(integers))
+  // println(solve(integers))
+  println(solve2(integers))
 }
