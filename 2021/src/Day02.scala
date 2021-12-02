470f9b53170da89bedf6cc04a209c269ae6acfbb
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-02 20:53:13 +0100

    [day2][part2] same with Tuple3

--- 2021/src/Day02.scala
+++ 2021/src/Day02.scala
@@ -4,32 +4,46 @@ object Day02 extends App {
     result._1 * result._2
   }
 
+  def solve2(movements: Array[Movement]): Int = {
+    val result = movements.foldLeft((0, 0, 0))((tuple, m) => m.move(tuple))
+    result._1 * result._2
+  }
+
   val input = scala.io.Source.fromFile("inputs/day02")
-  var lines = (try input.mkString finally input.close())
+  var lines = (try input.mkString
+  finally input.close())
     .split('\n')
     .map(_.split(' '))
     .map { case Array(movement, value) => (movement, value.toInt) }
-    .map((movement, value) => (movement, value) match {
-      case ("forward", _) => Forward(value)
-      case ("up", _) => Up(value)
-      case ("down", _) => Down(value)
+    .map({
+      case ("forward", v) => Forward(v)
+      case ("up", v)      => Up(v)
+      case ("down", v)    => Down(v)
     })
 
-  println(solve(lines))
+  // println(solve(lines))
+  println(solve2(lines))
 }
 
 trait Movement {
   def move(data: (Int, Int)): (Int, Int)
+  def move(data: (Int, Int, Int)): (Int, Int, Int)
 }
 
 case class Forward(x: Int) extends Movement {
   override def move(data: (Int, Int)): (Int, Int) = (data._1 + x, data._2)
+  override def move(data: (Int, Int, Int)): (Int, Int, Int) =
+    (data._1 + x, data._2 + (data._3 * x), data._3)
 }
 
 case class Up(y: Int) extends Movement {
   override def move(data: (Int, Int)): (Int, Int) = (data._1, data._2 - y)
+  override def move(data: (Int, Int, Int)): (Int, Int, Int) =
+    (data._1, data._2, data._3 - y)
 }
 
 case class Down(y: Int) extends Movement {
   override def move(data: (Int, Int)): (Int, Int) = (data._1, data._2 + y)
+  override def move(data: (Int, Int, Int)): (Int, Int, Int) =
+    (data._1, data._2, data._3 + y)
 }
