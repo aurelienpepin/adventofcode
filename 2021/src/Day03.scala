257caf2f07359ca12f26e29d497f2d7bacf0bff0
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-03 23:15:53 +0100

    [day3][part2] refactor and format

--- 2021/src/Day03.scala
+++ 2021/src/Day03.scala
@@ -1,4 +1,6 @@
 object Day03 extends App {
+  val toInt: (Array[Char]) => Int = word => Integer.parseInt(word.mkString, 2)
+
   def mostCommonBit(word: Array[Char]): Char = {
     word.groupBy(identity).toSeq.sortWith(_._1 > _._1).maxBy(_._2.size)._1
   }
@@ -7,27 +9,23 @@ object Day03 extends App {
     word.groupBy(identity).toSeq.sortWith(_._1 < _._1).minBy(_._2.size)._1
   }
 
-  def rating(
-      words: Array[Array[Char]]
-  )(criteria: (Array[Char]) => Char): Array[Char] = {
+  def rating(words: Array[Array[Char]])(bit: (Array[Char]) => Char): Array[Char] = {
     (0 to words(0).length - 1)
-      .foldLeft(words)((words, i) =>
-        words.filter(_(i) == criteria(words.map(_(i))))
-      )(0)
+      .foldLeft(words)((ws, i) => ws.filter(_(i) == bit(ws.map(_(i)))))(0)
   }
 
   def solve(words: Array[Array[Char]]): Int = {
-    var gamma = words.transpose.map(mostCommonBit).mkString
-    var epsilon = gamma.toCharArray.map('1' - _).mkString
+    val gamma = words.transpose.map(mostCommonBit)
+    val epsilon = gamma.map('1' - _).map(_.toChar)
 
-    Integer.parseInt(gamma, 2) * Integer.parseInt(epsilon, 2)
+    toInt(gamma) * toInt(epsilon)
   }
 
   def solve2(words: Array[Array[Char]]): Int = {
-    val oxygen = Integer.parseInt(rating(words)(mostCommonBit).mkString, 2)
-    val co2 = Integer.parseInt(rating(words)(leastCommonBit).mkString, 2)
+    val oxygen = rating(words)(mostCommonBit)
+    val co2 = rating(words)(leastCommonBit)
 
-    return oxygen * co2
+    toInt(oxygen) * toInt(co2)
   }
 
   val words = scala.io.Source
