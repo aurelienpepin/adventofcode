366a6ab209f6316087d1fcbd73caad58d2effcf9
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-06 22:45:54 +0100

    [day6][part2] fibonacci

--- 2021/src/Day06.scala
+++ 2021/src/Day06.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object Day06 extends App {
   def nextCycle(timers: Array[Int]): Array[Int] = {
     timers
@@ -10,12 +12,29 @@ object Day06 extends App {
       })
   }
 
+  def memoize[I, O](f: I => O): I => O = new mutable.HashMap[I, O]() {
+    override def apply(key: I) = getOrElseUpdate(key, f(key))
+  }
+
+  val fibo7_9: Int => BigInt = memoize { (n) =>
+    if (n < 0)
+      0
+    else if (n < 9)
+      1
+    else
+      fibo7_9(n - 7) + fibo7_9(n - 9)
+  }
+
   def solve(timers: Array[Int], days: Int): Int = {
     (0 until days)
       .foldLeft(timers)((ts, _) => nextCycle(ts))
       .length
   }
 
+  def solve2(timers: Array[Int], days: Int): BigInt = {
+    timers.map(t => fibo7_9(days + 8 - t)).reduce(_ + _)
+  }
+
   val digits = scala.io.Source.fromFile("inputs/day06").mkString.strip.split(',').map(_.toInt)
-  println(solve(digits, 80))
+  println(solve2(digits, 256))
 }
