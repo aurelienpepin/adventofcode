03e3dac7b59b89a072b6f67d7a54e82711f0b0bc
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-07 13:42:43 +0100

    [day7][part2] binary search

--- 2021/src/Day07.scala
+++ 2021/src/Day07.scala
@@ -3,11 +3,31 @@ object Day07 extends App {
     values.sorted.apply(values.length / 2)
   }
 
-  def solve(values: Array[Int]): Int = {
-    val med = median(values)
-    values.map(x => Math.abs(x - med)).sum
+  val fuel1 = (values: Array[Int], position: Int) => values.map(v => Math.abs(v - position)).sum
+  val fuel2 = (values: Array[Int], position: Int) =>
+    values.map(v => Math.abs(v - position) * (Math.abs(v - position) + 1)).sum / 2
+
+  def solve(values: Array[Int]): Int = fuel1(values, median(values))
+
+  def solve2(values: Array[Int], min: Int, max: Int): Int = {
+    if (max < min) {
+      Int.MaxValue
+    } else {
+      val mid = min + (max - min) / 2;
+
+      val left = fuel2(values, mid - 1)
+      val here = fuel2(values, mid)
+      val right = fuel2(values, mid + 1)
+
+      if (here < left && here < right)
+        here
+      else if (left < here)
+        solve2(values, min, mid - 1)
+      else
+        solve2(values, mid + 1, max)
+    }
   }
 
   val digits = scala.io.Source.fromFile("inputs/day07").mkString.strip.split(',').map(_.toInt)
-  println(solve(digits))
+  println(solve2(digits, digits.min, digits.max))
 }
