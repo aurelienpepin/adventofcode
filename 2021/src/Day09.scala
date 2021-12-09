477dc1dea99e5d9dc937fc691e3b61dbb05d8df3
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-09 13:28:52 +0100

    [day9][part2] BFS

--- 2021/src/Day09.scala
+++ 2021/src/Day09.scala
@@ -1,3 +1,5 @@
+import scala.collection.mutable
+
 object Day09 extends App {
   class Grid(var grid: Array[Array[Int]]) {
     def isLowPoint(x: Int, y: Int): Boolean = {
@@ -18,13 +20,44 @@ object Day09 extends App {
         .toArray
     }
 
-    def getBasinSize(x: Int, y: Int): Int = 0
+    def neighbours(point: (Int, Int)): Array[(Int, Int)] = {
+      val get: (Int, Int) => Option[(Int, Int)] = (x, y) =>
+        if (x < 0 || x == grid.length || y < 0 || y == grid(x).length) Option.empty else Some((x, y))
+
+      val x = point._1
+      val y = point._2
+      Array((x - 1, y), (x + 1, y), (x, y + 1), (x, y - 1)).map { case (x, y) => get(x, y) }.flatten
+    }
+
+    def getBasinSize(x: Int, y: Int): Int = {
+      if (!isLowPoint(x, y))
+        return 1
+
+      var queue = mutable.Queue((x, y))
+      var visited = mutable.Set[(Int, Int)]()
+      var size = 0
+
+      while (!queue.isEmpty) {
+        val point = queue.dequeue
+        if (!visited.contains(point)) {
+          visited.add(point)
+
+          neighbours(point).filterNot(p => grid(p._1)(p._2) == 9).foreach(queue.enqueue(_))
+          size += 1
+        }
+      }
+
+      size
+    }
   }
 
   def solve(grid: Grid): Int = grid.allPoints().map { case (x, y) => grid.riskLevel(x, y) }.sum
 
+  def solve2(grid: Grid): Int =
+    grid.allPoints().map { case (x, y) => grid.getBasinSize(x, y) }.sortBy(identity).reverse.take(3).product
+
   val input = scala.io.Source.fromFile("inputs/day09").mkString.split('\n').map(_.strip.map(_.asDigit).toArray)
   var grid = new Grid(input)
 
-  println(solve(grid))
+  println(solve2(grid))
 }
