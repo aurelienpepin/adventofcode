c8007a5ca6a5b17781be4fd197cb12877682fa99
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-10 18:45:15 +0100

    [day10][part2] stack.pop until empty + bigint

--- 2021/src/Day10.scala
+++ 2021/src/Day10.scala
@@ -2,7 +2,6 @@ import scala.collection.mutable.Stack
 
 object Day10 extends App {
   def solve(line: String): Int = {
-    // println("------")
     val stack = Stack[Char]()
 
     for (c <- line.toCharArray) {
@@ -10,17 +9,36 @@ object Day10 extends App {
         stack.push(c)
       else {
         if (stack.isEmpty || stack.head != matchingCharacter(c)) {
-          // println(c + " " + stack)
-          // println("illegal " + c)
           return score(c)
         } else
           stack.pop()
       }
-
-      // println(c + " " + stack)
     }
 
     0
+  }
+
+  def solve2(line: String): Option[BigInt] = {
+    val stack = Stack[Char]()
+
+    for (c <- line.toCharArray) {
+      if (isOpening(c))
+        stack.push(c)
+      else {
+        if (stack.isEmpty || stack.head != matchingCharacter(c)) {
+          return None // no point for corrupted lines
+        } else
+          stack.pop()
+      }
+    }
+
+    var totalScore: BigInt = 0
+    while (!stack.isEmpty) {
+      totalScore = totalScore * 5 + score2(matchingReverseCharacter(stack.head))
+      stack.pop()
+    }
+
+    Some(totalScore)
   }
 
   val isOpening = (c) => Set('(', '[', '{', '<').contains(c)
@@ -31,8 +49,19 @@ object Day10 extends App {
       case ']' => '['
       case '>' => '<'
     }
+  var matchingReverseCharacter = (c: Char) =>
+    c match {
+      case '(' => ')'
+      case '{' => '}'
+      case '[' => ']'
+      case '<' => '>'
+    }
   val score = Map((')' -> 3), (']' -> 57), ('}' -> 1197), ('>' -> 25137))
+  val score2 = Map((')' -> 1), (']' -> 2), ('}' -> 3), ('>' -> 4))
   val input = scala.io.Source.fromFile("inputs/day10").mkString.split('\n').map(_.strip)
 
-  println(input.map(solve(_)).sum)
+  // println(input.map(solve(_)).sum)
+
+  val res = input.map(solve2(_)).flatten.sorted
+  println(res(res.length / 2))
 }
