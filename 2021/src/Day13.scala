1fe6115a81ef076af80aff2f591c20e6e0e87c50
Aurélien Pepin <aurelien.pepin@blablacar.com>
2021-12-13 20:23:28 +0100

    [day13][part2] pattern matching again

--- 2021/src/Day13.scala
+++ 2021/src/Day13.scala
@@ -8,13 +8,57 @@ object Day13 extends App {
     }.size
   }
 
+  def solve2(points: Set[(Int, Int)], instructions: Array[(Int, String)]): Unit = {
+    val endPoints = instructions.foldLeft(points)((ps, instruction) =>
+      val largestX = ps.maxBy(_._1)._1
+      val largestY = ps.maxBy(_._2)._2
+
+      instruction match {
+        case (d, "x") if d >= largestX / 2 =>
+          ps.map {
+            case (x, y) if x > d => (d - (x - d), y)
+            case (x, y)          => (x, y)
+          }
+        case (d, "x") =>
+          ps.map {
+            case (x, y) if x < d  => (d - (d - x), y)
+            case (x, y)           => (x - d - 1, y)
+          }
+        case (d, "y") if d >= largestY / 2 =>
+          ps.map {
+            case (x, y) if y > d => (x, d - (y - d))
+            case (x, y) => (x, y)
+          }
+        case (d, "y") =>
+          ps.map {
+            case (x, y) if y < d => (x, d - (d - y))
+            case (x, y) => (x, y - d - 1)
+          }
+        case _ => throw new IllegalArgumentException
+      }
+    )
+
+    for (i <- 0 to endPoints.maxBy(_._2)._2) {
+      for (j <- 0 to endPoints.maxBy(_._1)._1) {
+        if (endPoints.contains((j, i))) {
+          print("*")
+        } else {
+          print(" ")
+        }
+      }
+      println("")
+    }
+  }
+
   val input = scala.io.Source
     .fromFile("inputs/day13")
     .mkString
     .split("\n\n")
 
   val points = input(0).strip.split('\n').map(_.split(',')).map { case Array(x, y) => (x.toInt, y.toInt) }.toSet
-  val instructions = input(1).strip.split('\n').map(_.split(' ').last.split('=')).map { case Array(c, d) => (c, d) }
+  val instructions =
+    input(1).strip.split('\n').map(_.split(' ').last.split('=')).map { case Array(c, d) => (d.toInt, c) }.toArray
 
-  println(solve(points, 655, Int.MaxValue))
+  // println(solve(points, 655, Int.MaxValue))
+  solve2(points, instructions)
 }
